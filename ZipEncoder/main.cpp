c1fc918d26d86f27f37ea85d6b986a040acf0e48
ShavelDev <nor.szaw@gmail.com>
2024-10-20 17:23:27 +0200

    Initial Commit

---
+++ ZipEncoder/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ZipEncoder
+//
+//  Created by Norbert Szawloga on 20/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
